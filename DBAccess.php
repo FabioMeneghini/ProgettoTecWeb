b4d7e7b5cc36ff543855c6167c5ed9aa55a9b4c8
Fabio Meneghini <fabio.meneghini.1@studenti.unipd.it>
2024-01-05 19:30:15 +0100

    aggiunte le modifiche di Nicola a style.css, aggiunto index.php (home page per utenti non registrati), aggiunto DBAccess.php con metodi per connessione, disconnessione e recupero lista best seller dal DB funzionanti

--- DBAccess.php
+++ DBAccess.php
@@ -1,90 +1,44 @@
 <?php
-    namespace DB;
-    class DBAccess {
-        private const HOST_DB = "localhost"; //qiesti valori sono da cambiare prima di provarlo sul server di unipd
-        private const DATABASE_NAME = "bookclub";
-        private const USERNAME = "root";
-        private const PASSWORD = "";
 
-        private $connection;
+namespace DB;
+class DBAccess {
+    private $HOST_DB = "localhost"; //questi valori sono da cambiare prima di provarlo sul server di unipd
+    private $DATABASE_NAME = "bookclub";
+    private $USERNAME = "root";
+    private $PASSWORD = "";
 
-        public function openDBConnection() {
-            $this -> connection = mysqli_connect(
-                self::HOST_DB,
-                self::USERNAME,
-                self::PASSWORD,
-                self::DATABASE_NAME
-            );
-            return mysqli_connect_errno() == 0;
-        }
+    private $connection;
 
-        public function closeConnection() {
-            mysqli_close($this -> connection);
-        }
-
-        /*
-        public function getListaAlbum() {
-            $query = "SELECT ID, Titolo, Copertina, idCss FROM Album ORDER BY DataPubblicazione DESC";
-            $queryResult = mysqli_query($this -> connection, $query) or die("Errore in DBAccess" . mysqli_error($this -> connection));
-            if(mysqli_num_rows($queryResult) != 0){
-                $result = array();
-                while($row = mysqli_fetch_assoc($queryResult)) {
-                    $result[] = $row;
-                }
-                $queryResult -> free();
-                return $result;
-            }else{
-                return null;
-            }
-        }
-        
-
-        public function getListaAlbum($id) {
-            $query = "SELECT Album.ID,
-                        Album.Titolo,
-                        Copertina,
-                        DataPubblicazione,
-                        SEC_TO_TIME(SUM(TIME_TO_SEC(Traccia.Durata))) as DurataAlbum
-                        FROM Album
-                        JOIN Traccia ON Album.ID = Traccia.DurataAlbum
-                        WHERE Album.ID = $id";
-            $queryResult = mysqli_query($this -> connection, $query) or die("Errore in DBAccess" . mysqli_error($this -> connection));
-            if(mysqli_num_rows($queryResult) != 0){
-                $row = mysqli_fetch_assoc($queryResult);
-                return array($row["ID"],
-                        $row["Titolo"],
-                        $row["Copertina"],
-                        $row["DataPubblicazione"],
-                        $row["DurataAlbum"]);
-            }else{
-                return null;
-            }
-        }
-
-        public function getTracceAlbum($id) {
-            $query = "SELECT Traccia.Titolo,
-                Traccia.Esplicito,
-                Traccia.Durata,
-                DataRadio,
-                URLVideo
-            FROM Traccia
-            JOIN Album ON Traccia.Album = Album.id
-            WHERE Traccia.Album = $id";
-        }
-
-        public insertNewTrack($album, $titotlo, $durata, $esplicito, $dataRadio, $urlVideo, $note) {
-            $queryInsert = "INSERT INTO Traccia(Titolo, Durata, Esplicito, URLVideo, DataRadio, Album, Note)
-                VALUES (\"$titolo\",
-                    \"$durata\",
-                    \"$esplicito\",
-                    NULLIF(\"$urlVideo\",\"\"),
-                    NULLIF(\"$dataradio\",\"\"), 
-                    NULLIF(\"$titolo\",\"\"), 
-                    NULLIF(\"$album\",\"\"), 
-                    NULLIF(\"$note\",\"\"));";
-            mysqli_query(&this->connection, &queryInsert) or die(mysqli_error($this->connection));
-            return mysqli_affected_rows(&this->connection) > 0;
-        }
-        */
+    public function openDBConnection() {
+        $this -> connection = mysqli_connect(
+            $this->HOST_DB,
+            $this->USERNAME,
+            $this->PASSWORD,
+            $this->DATABASE_NAME
+        );
+        return mysqli_connect_errno() == 0;
     }
+
+    public function closeConnection() {
+        mysqli_close($this -> connection);
+    }
+
+    public function getListaBestSeller() {
+        $query = "SELECT titolo, autore, genere FROM libri LIMIT 10";
+        $queryResult = mysqli_query($this -> connection, $query);
+        if(mysqli_num_rows($queryResult) != 0){
+            $result = array();
+            while($row = mysqli_fetch_assoc($queryResult)) {
+                $result[] = $row;
+            }
+            $queryResult -> free();
+            return $result;
+        }else{
+            return null;
+        }
+    }
+
+    
+}
+
 ?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once "DBAccess.php";
+use DB\DBAccess;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+setlocale(LC_ALL, 'it_IT');
+
+$paginaHTML = file_get_contents("template/templateHomeNonRegistrato.html");
+
+$listaBestSeller = "";
+
+try {
+    $connection = new DBAccess();
+    $connectionOk = $connection -> openDBConnection();
+    if($connectionOk) {
+        $resultListaBestSeller = $connection -> getListaBestSeller();
+        foreach($resultListaBestSeller as $libro) {
+            $listaBestSeller .= "<li>".$libro["autore"]." - ".$libro["titolo"]." - ".$libro["genere"]."</li>";
+        }
+    }
+} catch(Exception $e) {
+    echo "Errore: ".$e -> getMessage();
+}
+
+$connection -> closeConnection();
+
+$paginaHTML = str_replace("{listaBestSeller}", $listaBestSeller, $paginaHTML);
+echo $paginaHTML;
+
+?>
