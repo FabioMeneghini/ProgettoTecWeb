a30dcbe0462f69ecda055bf7692dbdb82d157164
Leonardo Baldo <baldoleonardo01@gmail.com>
2024-05-02 16:59:02 +0200

    pagina generi + image replace

---
+++ generi.php
@@ -0,0 +1,91 @@
+<?php
+
+include "config.php";
+
+require_once "DBAccess.php";
+use DB\DBAccess;
+
+$paginaHTML = file_get_contents("template/templateGeneri.html");
+$menu ="";
+//utenti
+$userMenu ='<dt><a href="utente.php"><span lang="en">Home</span></a></dt>
+    <dt><a href="stai_leggendo.php">Libri che stai leggendo</a></dt>
+    <dt><a href="terminati.php">Libri terminati</a></dt>
+    <dt><a href="da_leggere.php">Libri da leggere</a></dt>
+    <dt><a href="recensione.php">Aggiungi Recensione</a></dt>
+    <dt>Lista Generi:</dt>
+    {listaGeneri}
+    <dt><a href="statistiche.php">Statistiche</a></dt>
+    <dt><a href="area_personale.php">Area Personale</a></dt>
+    <dt><a href="cerca.php">Cerca</a></dt>';
+
+//admin
+$adminMenu = '<dt><a href="admin.php"><span lang="en">Home</span></a></dt>
+    <dt><a href="aggiungi_libro.php">Aggiungi un libro</a></dt>
+    <dt><a href="tutti_libri.php">Catalogo libri</a></dt>
+    <dt><a href="tutti_utenti.php">Archivio utenti</a></dt>
+    <dt>Categorie</dt>
+    {listaGeneri}
+    <dt><a href="area_personale.php">Area Personale</a></dt>
+    <dt><a href="cerca.php">Cerca</a></dt>';
+
+$NonRegistrato='<dt><a href="index.php"><span lang="en">Home</span></a></dt>
+                <dt>Categorie</dt>
+                {listaGeneri}
+                <dt><a href="accedi.php">Accedi</a></dt>
+                <dt><a href="registrati.php">Registrati</a></dt>
+                <dt><a href="cerca.php">Cerca</a></dt>';
+
+if(isset($_SESSION['admin'])) {
+    if($_SESSION['admin'] == 1) {
+        $menu = $adminMenu;
+    } 
+    else
+        $menu =$userMenu;
+}
+else {
+    $menu =$NonRegistrato;
+}
+
+
+$menuGeneri = "";
+$listaGeneri = "";
+$resultKeyword = "";
+
+try {
+    $connection = new DBAccess();
+    $connectionOk = $connection -> openDBConnection();
+    if($connectionOk) {
+        $resultGeneri = $connection -> getListaGeneri();
+        //$resultKeyword = $connection->getKeywordByGenere($genereSelezionato);
+        //TO DO DB
+        $connection -> closeConnection();
+        foreach($resultGeneri as $genere) { //per ogni genere, creo una lista di libri di quel genere
+            $menuGeneri .= '<dd><a href="genere.php?genere='.$genere["nome"].'">'.$genere["nome"].'</a></dd>';
+            $listaGeneri .= '<li><a id="'.$genere["nome"].'" href="genere.php?genere='.$genere["nome"].'">'.$genere["nome"].'</a></li>';
+        }
+    
+        /*if(!empty($resultKeyword)) {
+            foreach($resultKeyword as $keyword) {
+                $listaKeyword .= '<li>'.$keyword['keyword'].'</li>';
+            }
+        } else {
+            $listaKeyword = "Miglior genere";
+        }*/
+    }
+    else {
+        echo "Connessione fallita";
+    }
+}
+catch(Throwable $e) {
+    echo "Errore: ".$e -> getMessage();
+}
+
+//$paginaHTML = str_replace("{keyword}", $listaKeyword , $paginaHTML);
+$paginaHTML = str_replace("{menu}", $menu , $paginaHTML);
+$paginaHTML = str_replace("{Generi}", $listaGeneri, $paginaHTML);
+$paginaHTML = str_replace("{listaGeneri}", $menuGeneri, $paginaHTML);
+
+echo $paginaHTML;
+
+?>
