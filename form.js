283eaeef62827cf24e8b1aef02b39314ee992440
NicolaLazzarin <126665369+NicolaLazzarin@users.noreply.github.com>
2024-05-03 17:04:44 +0200

    modifiche javascript (non funzionanti)

--- form.js
+++ form.js
@@ -1,4 +1,4 @@
-function caricamento(){
+/*function caricamento(){
     let inputTitle = document.getElementById("titolo");
     inputTitle.onblur = function () {validateTitle(this)};
    
@@ -52,29 +52,58 @@ function removeChildInput(input){
     if (padre.children.length==2){
         padre.removeChild(padre.children[1]);
     }
-}
+}*/
 
-function showError(tag, stringa){
+function showError(tag, stringa, element){
     const padre=tag.parentNode;
-    const errore = document.createElement("strong");
+    const errore = document.createElement(element);
     errore.className = "errorSuggestion";
     errore.appendChild(document.createTextNode(stringa));
     padre.appendChild(errore);
-
 }
 /*a posto qui sotto*/ 
-function validazioneCerca(){
-    let stringa = document.getElementById("stringa");
-    let autore = document.getElementById("autore");
-    let genereSelezionato = document.getElementById("genereSelezionato");
+function validazioneCerca() {
+    let stringa = document.getElementById("stringa").value;
+    let autore = document.getElementById("autore").value;
+    //let genereSelezionato = document.getElementById("genereSelezionato").value;
     let lingua = document.getElementById("lingua");
+    const lingua_val=lingua.value;
 
-    if(stringa == "" && autore == "" && genereSelezionato == "" && lingua == "") {
-        //$messaggi_form = "<p>Inserisci almeno un parametro di ricerca</p>";
-        showError("Inserisci almeno un parametro di ricerca")
-        return false
+    console.log(stringa);
+    console.log(autore);
+    console.log(lingua);
+
+    if (stringa === "" && autore === "" /*&& genereSelezionato === ""*/ && lingua_val === "") {
+        showError(lingua.parentNode, "Inserisci almeno un parametro di ricerca", "p");
+        return false;
     }
-    return true
+    return true;
+}
+function controllaAccedi() { //da inserire eventualmente altri controlli su username e password
+    let messaggi = "";
+    let ok=true;
+    let username = document.getElementById("username");
+    const username_val=username.value;
+    let password = document.getElementById("password").value;
+
+    if(username_val == "") {
+        messaggi +="<li>Lo username non può essere vuoto</li>";
+        ok=false;
+    }
+    if(password == "") {
+        messaggi += "<li>La password non può essere vuota</li>";
+        ok=false;
+    }
+    if(username_val.length <= 2) {
+        messaggi += "<li>Lo username non può essere più corto di 3 caratteri</li>";
+        ok=false;
+    }
+    console.log(username_val.length);
+    alert(username_val.length);
+    if(!ok) {
+        showError(username.parentNode, messaggi, "ul");
+    }
+    return ok;
 }
 
 /*function validazioneForm(){
