7673ac4060951138950e87228e196d590ddf4ba5
Fabio Meneghini <fabio.meneghini.1@studenti.unipd.it>
2024-06-27 17:39:19 +0200

    ricambiate le credenziali del db

--- DBAccess.php
+++ DBAccess.php
@@ -2,10 +2,15 @@
 
 namespace DB;
 class DBAccess {
-    private $HOST_DB = "localhost";
+    /*private $HOST_DB = "localhost"; //SERVER TECWEB
     private $DATABASE_NAME = "famenegh";
     private $USERNAME = "famenegh";
-    private $PASSWORD = "einohn7yie1soaBu";
+    private $PASSWORD = "einohn7yie1soaBu";*/
+
+    private $HOST_DB = "localhost"; //SERVER LOCALE
+    private $DATABASE_NAME = "bookclub";
+    private $USERNAME = "root";
+    private $PASSWORD = "";
 
     private $connection;
 
